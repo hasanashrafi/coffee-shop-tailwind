d49a957fad6ee1abc9538343fedd6ec6abc7f66a
Hasan Ashrafi <yasashrafi@gmail.com>
2024-07-10 22:34:10 +0330

    customize colors box shadow and font family of project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brown": {
+          "100": "#ECE0D1",
+          "300": "#DBC1AC",
+          "600": "#967259",
+          "900": "#634832"
+        }
+      }
+    },
+
+    boxShadow: {
+      "light": "0px 1px 10px 0px rgba(0, 0, 0, 0.05)",
+      "dark": " 0px 1px 10px 0px rgba(0, 0, 0, 0.05)",
+    },
+
+    borderRadius: {
+      "4xl ": "2rem",
+    },
+    fontFamily: {
+      "Dana": "Dana",
+      "DanaMedium": "Dana Medium",
+      "DanaDemiBold": "Dana DemiBold",
+      "MorabbaLight": "Morabba light",
+      "MorabbaMedium": "Morabba Medium",
+      "MorabbaBold": "Morabba Bold",
+    }
   },
   plugins: [],
 }
